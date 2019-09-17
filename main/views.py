d6e355663a297fd5120e0a33c982765db1dc62d0
NourAlkandari <noor.alkanderi@gmail.com>
2019-09-17 12:46:58 +0300

    finished article details functionality

--- main/views.py
+++ main/views.py
@@ -4,9 +4,16 @@ from .models import Article
 # Create your views here.
 def articles_list(request):
 
-	articles = Article.objects.all()
+	
 	context = {
-		"articles" : articles,
+		"articles" : Article.objects.all(),
 	}
 	
 	return render(request, "articles_list.html", context)
+
+def article_details(request, article_id):
+    context = { 
+    "article" : Article.objects.get(id=article_id)
+    }
+
+    return render(request, 'article_details.html', context)
