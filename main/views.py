db69cbeb848b89d29397702ee4382856598847fe
NourAlkandari <noor.alkanderi@gmail.com>
2019-09-18 09:17:47 +0300

    finished creating an article functionality

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 def articles_list(request):
@@ -17,3 +18,17 @@ def article_details(request, article_id):
     }
 
     return render(request, 'article_details.html', context)
+
+def create_article(request):
+	form = ArticleForm()
+	if request.method == "POST":
+		form = ArticleForm(request.POST)
+		if form.is_valid():
+			article = form.save(commit=False)
+			article.author = request.user
+			article.save()
+			return redirect('article-details', article.id)
+
+	context = {"form" : form}
+
+	return render(request, "create_article.html", context)
