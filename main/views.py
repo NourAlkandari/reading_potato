5a14df15f991a0ba9adabb144ebcb41a3c5fd739
NourAlkandari <noor.alkanderi@gmail.com>
2019-09-19 11:36:55 +0300

    finished edit an article functionality

--- main/views.py
+++ main/views.py
@@ -1,23 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleForm
 
 # Create your views here.
-def articles_list(request):
+################--LIST_PAGE--########################
 
-	
+def articles_list(request):
 	context = {
 		"articles" : Article.objects.all(),
 	}
 	
 	return render(request, "articles_list.html", context)
 
-def article_details(request, article_id):
-    context = { 
-    "article" : Article.objects.get(id=article_id)
-    }
+################--DETAIL_PAGE--########################
 
-    return render(request, 'article_details.html', context)
+def article_details(request, article_id):
+	context = { 
+	"article" : Article.objects.get(id=article_id)
+	}
+
+	return render(request, 'article_details.html', context)
+
+################--CREATE_FORM--########################
 
 def create_article(request):
 	form = ArticleForm()
@@ -32,3 +36,27 @@ def create_article(request):
 	context = {"form" : form}
 
 	return render(request, "create_article.html", context)
+
+
+################--EDIT_FORM--########################
+
+def edit_article(request, article_id):
+
+	article = Article.objects.get(id=article_id)
+
+	form = ArticleForm(instance=article)
+
+	if request.method == "POST":
+		form = ArticleForm(request.POST, instance=article)
+
+		if form.is_valid():
+			form.save()
+			return redirect('article-details', article_id)
+
+
+	context = { "form" : form ,
+	"article" : article}
+	
+	
+
+	return render(request, 'edit_article.html', context)
